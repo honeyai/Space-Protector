89a00783fb40074eacaf755e02d54fcf8f1f3fa6
honeyai <contact.jessicasanchez@gmail.com>
2020-06-25 00:15:04 -0400

    TO FIX: when the player misses it goes to the next ship. I need to attack the same ship

--- script.js
+++ script.js
@@ -17,29 +17,71 @@ let alienFP = randomizer(2,4)
 let alienACC = randomizer(0.6, 0.8)
 //randomly generated range between .6 n .8
 
+//index to hold alien Ship names
+let index;
+
+
+
+let alienShipNames = ["The Pisdim", "The Lorin", "The Noro", "The Cabilval", "The Talgis", "The Nusti"]
+
+let getAlienName = () => index = Math.floor(Math.random()*alienShipNames.length);
+
 
 //*Ship object constructor
 
 class Ship {
-    constructor(hull, firepower, accuracy){ // might not need to feed these parameters
-    this.hull = hull;
+    constructor(name, hull, firepower, accuracy){ // might not need to feed these parameters
+    this.name = name,
+    this.hull = hull,
     this.firepower = firepower,
     this.accuracy = accuracy
   }
   //Adding in only the attack method because only the player will have an option to retreat
-  attack(){
+  attack(attacked, attacker){
+
+    console.log(`You attack the ${alienShip.name}!`)
+
     //create random number for attack between 0 and 1
     let hitChance = Math.random();
     (hitChance).toFixed(1);
-    //
+    //if hitChance is less than accuracy = miss
+    if(hitChance < this.accuracy){
+      console.log(`${attacker.name}'s attack misses!`)
+      //keep attacking the current enemy ship
+      // let currentShipIndex = index;
+
+    } else {
+      let damage = attacker.firepower;
+      console.log(`${attacked.name} takes a hit. Hull takes ${damage} damage.`);
+
+      console.log(`these are the remaining ships`,alienShipNames)
+      console.log(`this is the enemy ship:`,attacked.name)
+
+      //if the damage is more or equal to the hull then alien ship is destroyed and a new one is made.
+      if (damage >= attacked.hull){
+        console.log(`${attacked.name}'s hull shatters.`)
+        console.log("The fleet deploys a new ship to attack you!")
+        //use the same alien ship object but change it's name
+        //pop the previously defeated alien Ship
+        alienShipNames.splice(index);
+        console.log(`Updated array`,alienShipNames)
+        alienShip.name = alienShipNames[getAlienName()]
+        console.log(`${alienShip.name} approaches.`)
+      }
+    }
   }
 }
 
 //*Generating the Ships
+//I could also assign the player ship to have it's own unique name by story the prompt value into a variable. Test's for strings.
+const ussSchwarzenegger = new Ship("USS Schwarzenegger", 20, 5, 0.7)
 
-const ussSchwarzenegger = new Ship(20, 5, 0.7)
+const alienShip = new Ship(alienShipNames[getAlienName()], alienHull, alienFP, alienACC) 
 
-const alienShip = new Ship(alienHull, alienFP, alienACC) 
+
+//I could create an array of alien ship objects that each have a unique name when made...?
+//List of alien ship names
+
 
 //adding the retreat to out player ship
 ussSchwarzenegger.retreat = function (){
@@ -49,8 +91,7 @@ ussSchwarzenegger.retreat = function (){
   //if no => continue on with the game state. 
 }
 
-//calling retreat
-console.log(ussSchwarzenegger.attack());
+ussSchwarzenegger.attack(alienShip, ussSchwarzenegger);
 
 //Initializing values on game start and restart
 // let ussSchwarzenegger.hull = 20
@@ -59,5 +100,6 @@ console.log(ussSchwarzenegger.attack());
 
 
 //Creating game play 
+//maybe loop until names.length === 0
 
 //Getting the game play to the DOM
