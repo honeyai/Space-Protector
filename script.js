9a7bf46271c73d40374004de25149a643f3c1238
honeyai <contact.jessicasanchez@gmail.com>
2020-06-25 18:15:20 -0400

    All? basic functions of the game work

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//=======================
+//====How
+
+
+
 //* -------------------------------------------------------------Functions and variables
 
 //creating a function that will create a random number for alien ship's properties that come from a range
@@ -24,6 +29,10 @@ let index;
 
 //next turn
 let turnEnds;
+
+let damage;
+
+let remainingHull;
 
 let alienShipNames = ["The Pisdim", "The Lorin", "The Noro", "The Cabilval", "The Talgis", "The Nusti"]
 
@@ -52,10 +61,11 @@ class Ship {
       alert(`${attacker.name}'s attack misses!`)
       return turnEnds = true;
     } else { //otherwise on a hit
-      let damage = attacker.firepower;
+      damage = attacker.firepower;
       //if the damage is more or equal to the hull then alien ship is destroyed and a new one is made.
       if (damage >= attacked.hull){ 
         if(this.name === "USS Schwarzenegger"){
+          alert(`${attacked.name} takes a hit, hull takes ${damage} damage.`)
           alert(`${attacked.name}'s hull shatters!`)
           alert("The fleet deploys a new ship to attack you!")
           alienShipNames.splice(index,index); //removing the ship you just destroyed
@@ -63,20 +73,21 @@ class Ship {
           console.log(`this is the index at 70:`, index)
           alienShip = new Ship(alienShipNames[getAlienName()], alienHull, alienFP, alienACC) 
           alert(`${alienShip.name} approaches.`)
-          return turnEnds = true;
+          return turnEnds = false;
         } else if (this.name === alienShipNames[index]){
-          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. The ship's alarms begin to blare. The ship is going down! ${attacked.name} never makes it home. \n Game over.`)
-          //call for game over function.
+          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. The ship's alarms begin to blare. The ship is going down! ${attacked.name} never makes it home. \nGame over.`)
+          gameEnd();
         }
       } else {
         if(this.name === "USS Schwarzenegger"){
-
-          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. \n But it's still standing. You see it's lasers warming up, it fires!`);
+          remainingHull = attacked.hull - damage;
+          attacked.hull = remainingHull;
+          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. \nBut it's still standing. You see it's lasers warming up, it fires!`);
         } else if (this.name === alienShipNames[index]){
-          let remainingHull = attacked.hull - damage;
-          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. \n You're hull is at ${remainingHull}`)
+          remainingHull = attacked.hull - damage;
+          attacked.hull = remainingHull;
+          alert(`${attacked.name} takes a hit, hull takes ${damage} damage. \nYou're hull is at ${attacked.hull}`)
         }
-        
       }
     }
   }
@@ -84,29 +95,19 @@ class Ship {
 
 //*------------------------------------------------------------------Generating the Ships
 
-//I could also assign the player ship to have it's own unique name by storing the prompt value into a variable. Test's for strings.
-
-//let playerName = prompt("jsjsjd", USS Schwarzenegger) blah blah blah
-
 const ussSchwarzenegger = new Ship("USS Schwarzenegger", 20, 5, 0.7)
 
 var alienShip = new Ship(alienShipNames[getAlienName()], alienHull, alienFP, alienACC) 
 
-//I could create an array of alien ship objects that each have a unique name when made...
-
-//player attack
-
 //*------------------------------------------------------------------Beginning the game
 
-
-// let answer = prompt(`You're hull is at ${ship.hull}, and you still see ${alienShipNames.length} ships left. Do you [a]ttack or [r]etreat`);
-
 let answer;
-
+let quit;
 const gameStart = () => {
   alert("You are the USS Schwarzenegger. On your return flight back to Earth you're met with a barricade of alien ships. Defeat them to get home!")
   alert(`${alienShip.name} approaches.`)
-  answer = prompt("You're hull is at \`${ship.hull}\` and you still see ships left. Do you [a]ttack or [r]etreat");
+  // answer = prompt(`${ussSchwarzenegger.hull}`);
+  answer = prompt(`You're hull is at ${ussSchwarzenegger.hull}, and you still see ${alienShipNames.length} ships left. Do you [a]ttack or [r]etreat`);
   gameContinue();
 }
 
@@ -119,14 +120,16 @@ const gameEnd = () =>{
 }
 
 const gameContinue = () => {
-  while (answer === "a"){
-    ussSchwarzenegger.attack(alienShip,ussSchwarzenegger);//I should return if the player turn ends to then start the next attack'
+  while (answer === "a" || quit === null){
+    ussSchwarzenegger.attack(alienShip, ussSchwarzenegger);//I should return if the player turn ends to then start the next attack'
     if (turnEnds === true){
       alienShip.attack(ussSchwarzenegger, alienShip);
+    } else if (turnEnds === false ) {
+      answer = prompt(`You're hull is at ${ussSchwarzenegger.hull}, and you still see ${alienShipNames.length} ships left. Do you [a]ttack or [r]etreat`);
     }
   } 
   if (answer === "r" || answer === undefined || answer === null) {
-    let quit = confirm(`Are you sure you want to retreat?`);
+     quit = confirm(`Are you sure you want to retreat?`);
     if (quit === true){
       gameEnd();
     } else if (quit === null){
@@ -147,8 +150,11 @@ gameStart();
 
 //!=========================================== NOTES ================================================
 // ! Now what's happening is I get it actually do what I need expect, for when at the end. It's gunna give me an undefined alien. but because technically it works imma commit.
-//! It is not doing what i wanted. it's not generating different values for alien ship and that's cuz it's not creating a new class.
-//! Have to make conditionals for different alerts depending on who's attacking
+// ! It is not doing what i wanted. it's not generating different values for alien ship and that's cuz it's not creating a new class.
+// ! Have to make conditionals for different alerts depending on who's attacking
+
+//*======================================= CURRENT EDITION ==========================================
+//*Committing because the general gist of the game works
 
 //TODO After everything works turn if else's into ternarys...maybe
 
